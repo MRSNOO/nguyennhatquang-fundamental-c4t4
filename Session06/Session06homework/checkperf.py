835bcd6d5eeaa0eee183e90310467018eb711e84
MRSNOO <nnquang.1412@gmail.com>
2018-07-11 17:22:36 +0700

    initial commit

--- Session06/Session06homework/checkperf.py
+++ Session06/Session06homework/checkperf.py
@@ -3,7 +3,7 @@ s= 0
 for i in range(1,numb_in,1):
     if numb_in %i == 0 :
         s=s+i
-if numb_in == s :
+if numb_in == s and numb_in > 0 :
     print("{0} is a perfect number".format(numb_in))
 else :
     print("{0} is not a perfect number".format(numb_in))
