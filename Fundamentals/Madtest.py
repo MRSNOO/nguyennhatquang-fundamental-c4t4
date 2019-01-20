ab3724166c9e560c74e63315d19bed1756ae2afb
MRSNOO <nnquang.1412@gmail.com>
2019-01-20 15:26:29 +0700

    initial commit

--- Fundamentals/Madtest.py
+++ Fundamentals/Madtest.py
@@ -1,66 +1,71 @@
 map = {
-    "size_x":4,
-    "size_y":4
+    "size_x":5,
+    "size_y":5
 
 }
-player = {"x": 1,"y":1}
-boxes = {"x":2, "y":2}
-storages = {"x":2,"y":3}
+player = {"x": 1,"y":2}
+enemies = [{"x":3,"y":0},{"x":3,"y":3}]
+island = {"x":2,"y":2}
 playing = True 
+enemie_on_island = 0
 while playing : 
     for y in range (map["size_y"]):
         for x in range(map["size_x"]):
-            player_is_here = False 
-            if x==player["x"] and y == player["y"]:
-                player_is_here = True
-
-            box_is_here = False 
-            if x == boxes["x"] and y == boxes["y"]:
-                box_is_here = True
             
-            storage_is_here = False
-            if x == storages["x"] and y == storages["y"]:
-                storage_is_here = True
+            player_is_here_doc = False 
+            if x==player["x"] and y == player["y"]:
+                player_is_here_doc = True
 
-            if player_is_here == True:
-                print("P ",end = " ")
-            elif box_is_here == True:
-                print("B ",end = " ")
-            elif storage_is_here == True:
+            enemies_is_here = False 
+            for enemie in enemies :
+                if x == enemie["x"] and y == enemie["y"]:
+                    enemies_is_here = True
+            
+            island_is_here = False
+            if x == island["x"] and y == island["y"]:
+                island_is_here = True
+
+            if player_is_here_doc == True:
+                print("| ",end = " ")
+            elif (enemies_is_here == True and enemie_on_island == 0):
+                print("E ",end = " ")
+            elif island_is_here == True:
                 print("S ",end = " ")
             else :
-                print("- ",end = " ")
+                print("x ",end = " ")
         print() #xuong dong 
-    win = True
-    for box in boxes: 
-        if box not in storages: 
-            win = False 
     
-    if win == True : 
-        print("YOU WON !!")
-        break
-    for box in boxes :
-        if(box["x"] == 0 and box["y"] == map["size_y"]-1) \
-        or (box["x"] == 0 and box["y"] == 0)\
-        or (box["x"]== map["size_x"]-1 and box["y"] == 0)\
-        or (box["x"]==map["size_x"]-1 and box["y"] == map["size_y"]-1):
-            playing = False 
-            print("You Lose")
-    move = input("Your next move? A/W/S/D").lower() 
+    # for box in boxes :
+    #     if(box["x"] == 0 and box["y"] == map["size_y"]-1) \
+    #     or (box["x"] == 0 and box["y"] == 0)\
+    #     or (box["x"]== map["size_x"]-1 and box["y"] == 0)\
+    #     or (box["x"]==map["size_x"]-1 and box["y"] == map["size_y"]-1):
+    #         playing = False 
+    #         print("You Lose")
+
+    
+    move = input("Your next move? A/W/S/D or Fire(F)").lower() 
 
     dx = 0
     dy = 0
-    if (move == "A"): 
+    if (move == "a"): 
         dx = dx - 1
-    elif (move == "W"):
+    elif (move == "w"):
         dy = dy - 1 
-    elif (move == "S"):
+    elif (move == "s"):
         dy = dy + 1 
-    elif (move == "D"):
+    elif (move == "d"):
         dx = dx + 1 
+    elif (move == "f"):
+        if (player_is_here_doc == True) :
+            for i in range(map["size_x"]):
+                for enemie in enemies:
+                    if (i == enemie["x"]) : 
+                        enemie["x"] = player["x"]
+                        enemie["y"] = player["y"]                                                     
     else : 
         playing = False 
-    
+
     xmove = player["x"] + dx 
     ymove = player["y"] + dy 
 
@@ -68,20 +73,25 @@ while playing :
     if ((0<= xmove < map["size_x"]) and (0<= ymove <map["size_y"])) : 
         player["x"] += dx 
         player["y"] += dy 
+        for enemie in enemies:
+            enemie["x"] += dx
+            enemie["y"] += dy
+            if(enemie["x"]>=map["size_x"]):
+                enemie["x"] -= 2*dx
+            elif(enemie["y"]>=map["size_y"]):
+                enemie["y"] -= 2*dy
+            elif(enemie["x"]<0):
+                enemie["x"] -= 2*dx
+            elif(enemie["y"]<0):
+                enemie["y"] -= 2*dy
+            # elif(enemie["x"]==island["x"] and enemie["y"]==island["y"]):
+            #     enemie_on_island = 1
 
-    for box in boxes :
-        if box["x"] == player["x"] and box["y"] == player["y"]:
-            box["x"] += dx 
-            box["y"] += dy 
-        if box["x"]>=map["size_x"] or box["x"]<0 or box["y"]>=map["size_y"] or box["y"]<0:
-            player["x"] -= dx
-            player["y"] -= dy
-            box["x"] -= dx    
-            box["y"] -= dy
+
+        
+    
         
             
         
     
-
-
-            
+          
